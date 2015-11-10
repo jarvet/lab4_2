25a5f8d78abbc0c927d2756b666c75e98916e506
Jarvet <zgw122103119@163.com>
2015-11-10 22:46:23 +0800

    Update models.py

--- models.py
+++ models.py
@@ -1,17 +1,5 @@
 from django.db import models
 
-# Create your models here.
-
-class Author(models.Model):
-    Name = models.CharField(max_length=50, null=True)
-    Age = models.IntegerField(null=True)
-    Country = models.CharField(max_length=200, null=True)
-
-
-    class Meta:
-        ordering = ['Name', 'Age', 'Country']
-
-
 class Book(models.Model):
     ISBN = models.CharField(max_length=50, primary_key=True)
     Title = models.CharField(max_length=500, null=True)
